3cc5c50f77861ca29eeb8d72bc8937aa30dabdf5
Sayan SUOS <sayan.suos@ecam-ldb.com>
2026-07-08 14:49:27 +0200

    modif main

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ if __name__ == "__main__":
         )
 
     elif MODE == "train":
-        run_train(experiments=train_experiments[6:7], previous_exp=train_experiments[5])
-        run_validation(experiments=train_experiments[6:7], n_episodes=100, n_render=5)
+        run_train(experiments=train_experiments[8:9], previous_exp=train_experiments[7])
+        run_validation(experiments=train_experiments[8:9], n_episodes=100, n_render=5)
         # run_train(experiments=train_experiments, previous_exp=None)
         # run_validation(experiments=train_experiments, n_episodes=100, n_render=5)
 
