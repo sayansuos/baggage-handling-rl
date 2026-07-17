44d78078cd3db6b9790c2e9ce065d37c1a70f953
Sayan SUOS <sayan.suos@ecam-ldb.com>
2026-07-17 16:54:37 +0200

    update training

--- main.py
+++ main.py
@@ -37,11 +37,11 @@ if __name__ == "__main__":
         )
 
     elif MODE == "train":
-        run_train(experiments=train_experiments[4:5], previous_exp=train_experiments[3])
-        run_validation(experiments=train_experiments[4:5], n_episodes=100, n_render=5)
+        run_train(experiments=train_experiments[:6], previous_exp=None)
+        run_validation(experiments=train_experiments[:6], n_episodes=100, n_render=5)
 
     elif MODE == "validation":
-        run_validation(experiments=train_experiments, n_episodes=500, n_render=5)
+        run_validation(experiments=train_experiments[:1], n_episodes=100, n_render=5)
 
     elif MODE == "evaluation":
         run_evaluation(
